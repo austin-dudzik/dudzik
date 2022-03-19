baca96d22d7b643d5a85a2f2739959e0e11610dc
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-19 02:59:14 -0500

    Added projects

---
+++ projects.php
@@ -0,0 +1,228 @@
+<?php require_once "classes/Loader.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<?= Loader::getHead('Projects') ?>
+<body>
+<?= Loader::getExtras() ?>
+
+<?= Loader::getNavigation('Projects') ?>
+
+<div class="container pt-5">
+
+    <section class="my-5 mt-5 pt-2">
+
+
+                <div class="bg-black text-center mt-5">
+                    <h1 class="mb-3 fw-900 mt-0">Projects</h1>
+                    <p class="text mt-0 text-muted mb-0">I love building new things and have been designing, developing,
+                        and deploying new projects and applications over the years, ranging from full-fledged
+                        social media platforms to feedback tracking tools that help site owners track website
+                        feedback.</p>
+                </div>
+
+        <hr class="my-5">
+
+
+
+
+        <div class="row mt-4">
+
+            <div class="col-md-6 mb-4">
+                <a href="#" class="card hover p-0">
+                    <img src="assets/img/projects/covers/nova.png" alt="Nova cover"
+                         class="img-fluid w-100 rounded-top">
+                    <img width="75" height="75" src="assets/img/projects/icons/nova.png"
+                         alt="DomainKit" class="icon ms-4">
+                    <div class="p-4 pb-5 pt-3 pb-2">
+                        <div class="title">Nova</div>
+                        <div class="subtitle text-muted mb-2">User feedback collection
+                            platform
+                        </div>
+                        <div class="d-flex ps-1">
+                            <img class="me-3" src="https://img.icons8.com/ios-filled/20/ffffff/html-5--v1.png">
+                            <img class="me-3" src="https://img.icons8.com/ios-filled/20/ffffff/css3.png">
+                            <img class="me-3" src="https://img.icons8.com/ios-filled/20/ffffff/php-logo.png">
+                            <img class="me-3" src="https://img.icons8.com/ios-filled/20/ffffff/javascript.png">
+                            <img class="me-3" src="https://img.icons8.com/ios-filled/20/ffffff/jquery.png">
+                            <img class="me-3" src="https://img.icons8.com/material-outlined/20/ffffff/mysql-logo.png">
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-6 mb-4">
+                <a href="#" class="card hover p-0">
+                    <img src="assets/img/projects/covers/spark.png" alt="Nova cover"
+                         class="img-fluid w-100 rounded-top">
+                    <img width="75" height="75" src="assets/img/projects/icons/spark.png"
+                         alt="DomainKit" class="icon ms-4">
+                    <div class="p-4 pb-5 pt-3 pb-2">
+                        <div class="title">Spark</div>
+                        <div class="subtitle text-muted mb-2">Task management platform
+                        </div>
+                        <img class="me-2" src="https://img.icons8.com/ios/20/ffffff/laravel.png" data-toggle="tooltip" data-placement="top" title="Laravel">
+                        <img class="me-2" src="https://img.icons8.com/ios-glyphs/20/ffffff/css3.png" data-toggle="tooltip" data-placement="top" title="CSS">
+                        <img class="me-2" src="https://img.icons8.com/ios-filled/20/ffffff/php-logo.png" data-toggle="tooltip" data-placement="top" title="PHP">
+                        <img class="me-2" src="https://img.icons8.com/material-outlined/20/ffffff/mysql-logo.png" data-toggle="tooltip" data-placement="top" title="MySQL">
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-6 mb-4">
+                <a href="#" class="card hover p-0">
+                    <img src="assets/img/projects/covers/insight.png" alt="Insight cover"
+                         class="img-fluid w-100 rounded-top">
+                    <img width="75" height="75" src="assets/img/projects/icons/insight.png"
+                         alt="DomainKit" class="icon ms-4">
+                    <div class="p-4 pb-5 pt-3 pb-2">
+                        <div class="title">Insight</div>
+                        <div class="subtitle text-muted mb-2">User feedback collection
+                            platform
+                        </div>
+                        <img class="me-2" src="https://img.icons8.com/windows/20/ffffff/oracle-logo.png" data-toggle="tooltip" data-placement="top" title="Oracle SQL">
+                        <img class="me-2" src="https://img.icons8.com/ios-filled/20/ffffff/c-sharp-logo.png" data-toggle="tooltip" data-placement="top" title="C#">
+                        <img class="me-2" src="https://img.icons8.com/external-tal-revivo-regular-tal-revivo/20/ffffff/external-json-javascript-object-notation-is-a-lightweight-data-interchange-format-logo-regular-tal-revivo.png" data-toggle="tooltip" data-placement="top" title="JSON">
+                        <img class="me-2" src="https://img.icons8.com/ios-filled/20/ffffff/jquery.png" data-toggle="tooltip" data-placement="top" title="jQuery">
+                        <span class="small text-white">+ 5 more</span>
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-6 mb-4">
+                <a href="#" class="card hover p-0">
+                    <img src="assets/img/projects/covers/nova.png" alt="Nova cover"
+                         class="img-fluid w-100 rounded-top">
+                    <img width="75" height="75" src="assets/img/projects/icons/coordinator.png"
+                         alt="DomainKit" class="icon ms-4">
+                    <div class="p-4 pb-5 pt-3 pb-2">
+                        <div class="title">Coordinator</div>
+                        <div class="subtitle text-muted mb-2">User feedback collection
+                            platform
+                        </div>
+                        <img class="me-2" src="https://img.icons8.com/ios-filled/20/ffffff/wordpress--v1.png" data-toggle="tooltip" data-placement="top" title="WordPress">
+                        <img class="me-2" src="https://img.icons8.com/ios-glyphs/20/ffffff/css3.png" data-toggle="tooltip" data-placement="top" title="CSS">
+                        <img class="me-2" src="https://img.icons8.com/ios-filled/20/ffffff/php-logo.png" data-toggle="tooltip" data-placement="top" title="PHP">
+                        <img class="me-2" src="https://img.icons8.com/material-outlined/20/ffffff/mysql-logo.png" data-toggle="tooltip" data-placement="top" title="MySQL">
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-6 mb-4">
+                <a href="#" class="card hover p-0">
+                    <img src="assets/img/projects/covers/lirwc.png" alt="Nova cover"
+                         class="img-fluid w-100 rounded-top">
+                    <img width="75" height="75" src="assets/img/projects/icons/lirwc.png"
+                         alt="DomainKit" class="icon ms-4">
+                    <div class="p-4 pb-5 pt-3 pb-2">
+                        <div class="title">LIRWC</div>
+                        <div class="subtitle text-muted mb-2">User feedback collection
+                            platform
+                        </div>
+                        <img class="me-2" src="https://img.icons8.com/ios-filled/20/ffffff/wordpress--v1.png" data-toggle="tooltip" data-placement="top" title="WordPress">
+                        <img class="me-2" src="https://img.icons8.com/ios-glyphs/20/ffffff/css3.png" data-toggle="tooltip" data-placement="top" title="CSS">
+                        <img class="me-2" src="https://img.icons8.com/ios-filled/20/ffffff/php-logo.png" data-toggle="tooltip" data-placement="top" title="PHP">
+                        <img class="me-2" src="https://img.icons8.com/material-outlined/20/ffffff/mysql-logo.png" data-toggle="tooltip" data-placement="top" title="MySQL">
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-6 mb-4">
+                <a href="#" class="card hover p-0">
+                    <img src="assets/img/projects/covers/eric_carle_homepage.png" alt="Nova cover"
+                         class="img-fluid w-100 rounded-top">
+                    <img width="75" height="75" src="assets/img/projects/icons/eric_carle_homepage.png"
+                         alt="DomainKit" class="icon ms-4">
+                    <div class="p-4 pb-5 pt-3 pb-2">
+                        <div class="title">Eric Carle</div>
+                        <div class="subtitle text-muted mb-2">User feedback collection
+                            platform
+                        </div>
+                        <img class="me-2" src="https://img.icons8.com/ios-filled/20/ffffff/wordpress--v1.png" data-toggle="tooltip" data-placement="top" title="WordPress">
+                        <img class="me-2" src="https://img.icons8.com/ios-glyphs/20/ffffff/css3.png" data-toggle="tooltip" data-placement="top" title="CSS">
+                        <img class="me-2" src="https://img.icons8.com/ios-filled/20/ffffff/php-logo.png" data-toggle="tooltip" data-placement="top" title="PHP">
+                        <img class="me-2" src="https://img.icons8.com/material-outlined/20/ffffff/mysql-logo.png" data-toggle="tooltip" data-placement="top" title="MySQL">
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-6 mb-4">
+                <a href="#" class="card hover p-0">
+                    <img src="assets/img/projects/covers/nova.png" alt="Nova cover"
+                         class="img-fluid w-100 rounded-top">
+                    <img width="75" height="75" src="assets/img/projects/icons/crinkle.png"
+                         alt="DomainKit" class="icon ms-4">
+                    <div class="p-4 pb-5 pt-3 pb-2">
+                        <div class="title">Crinkle</div>
+                        <div class="subtitle text-muted mb-2">User feedback collection
+                            platform
+                        </div>
+                        <img class="me-2" src="https://img.icons8.com/ios-filled/20/ffffff/html-5--v1.png" data-toggle="tooltip" data-placement="top" title="HTML">
+                        <img class="me-2" src="https://img.icons8.com/ios-glyphs/20/ffffff/css3.png" data-toggle="tooltip" data-placement="top" title="CSS">
+                        <img class="me-2" src="https://img.icons8.com/ios-filled/20/ffffff/php-logo.png" data-toggle="tooltip" data-placement="top" title="PHP">
+                        <img class="me-2" src="https://img.icons8.com/material-outlined/20/ffffff/mysql-logo.png" data-toggle="tooltip" data-placement="top" title="MySQL">
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-6 mb-4">
+                <a href="#" class="card hover p-0">
+                    <img src="assets/img/projects/covers/campfire.png" alt="Nova cover"
+                         class="img-fluid w-100 rounded-top">
+                    <img width="75" height="75" src="assets/img/projects/icons/campfire.png"
+                         alt="DomainKit" class="icon ms-4">
+                    <div class="p-4 pb-5 pt-3 pb-2">
+                        <div class="title">Campfire</div>
+                        <div class="subtitle text-muted mb-2">Website feeedback tool
+                        </div>
+                        <img class="me-2" src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/20/ffffff/external-bootstrap-a-free-and-open-source-css-framework-logo-bold-tal-revivo.png" data-toggle="tooltip" data-placement="top" title="Bootstrap">
+                        <img class="me-2" src="https://img.icons8.com/ios-filled/20/ffffff/jquery.png" data-toggle="tooltip" data-placement="top" title="jQuery">
+                        <img class="me-2" src="https://img.icons8.com/ios-filled/20/ffffff/php-logo.png" data-toggle="tooltip" data-placement="top" title="PHP">
+                        <img class="me-2" src="https://img.icons8.com/ios-filled/20/ffffff/javascript.png" data-toggle="tooltip" data-placement="top" title="JavaScript">
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-6">
+                <a href="#" class="card hover p-0">
+                    <img src="assets/img/projects/covers/dune.png" alt="Dune cover"
+                         class="img-fluid w-100 rounded-top">
+                    <img width="75" height="75" src="assets/img/projects/icons/dune.png"
+                         alt="DomainKit" class="icon ms-4">
+                    <div class="p-4 pb-4 pt-3 pb-2">
+                        <div class="title">Dune</div>
+                        <div class="subtitle text-muted mb-2">Social media platform for Spotify
+                        </div>
+                        <img class="me-2" src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/20/ffffff/external-firebase-a-googles-mobile-platform-that-helps-you-quickly-develop-high-quality-apps-logo-bold-tal-revivo.png" data-toggle="tooltip" data-placement="top" title="Firebase">
+                        <img class="me-2" src="https://img.icons8.com/windows/20/ffffff/vuejs.png" data-toggle="tooltip" data-placement="top" title="VueJS">
+                        <img class="me-2" src="https://img.icons8.com/ios-glyphs/20/ffffff/spotify.png" data-toggle="tooltip" data-placement="top" title="Spotify API">
+                        <img class="me-2" src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/20/ffffff/external-bootstrap-a-free-and-open-source-css-framework-logo-bold-tal-revivo.png" data-toggle="tooltip" data-placement="top" title="Bootstrap">
+                    </div>
+                </a>
+            </div>
+
+            <div class="col-md-6">
+                <a href="#" class="card hover p-0">
+                    <img src="assets/img/projects/covers/yale.png"
+                         alt="Dune cover" class="img-fluid w-100 rounded-top">
+                    <img width="75" height="75"
+                         src="assets/img/projects/icons/yale.png" alt="DomainKit"
+                         class="icon ms-4">
+                    <div class="p-4 pb-4 pt-3 pb-2">
+                        <div class="title">Yale SoA</div>
+                        <div class="subtitle text-muted mb-2">Mock website redesign</div>
+                        <img class="me-2" src="https://img.icons8.com/material/20/ffffff/adobe-xd.png" data-toggle="tooltip" data-placement="top" title="Adobe XD">
+                    </div>
+                </a>
+            </div>
+
+        </div>
+
+    </section>
+
+    <?= Loader::getFooter() ?>
+
+</div>
+
+<?= Loader::getScripts() ?>
+</body>
+</html>
