0f5a4ae24323cfee300616852ddc05a7f1417a4a
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-19 02:59:25 -0500

    Added experience

---
+++ experience.php
@@ -0,0 +1,219 @@
+<?php require_once "classes/Loader.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+<?= Loader::getHead('Experience') ?>
+<body>
+<?= Loader::getExtras() ?>
+
+<?= Loader::getNavigation('Experience') ?>
+
+<div class="container pt-5">
+
+    <section class="my-5 mt-5 pt-2">
+
+
+        <div class="bg-black text-center mt-5">
+            <h1 class="mb-3 fw-900 mt-0">Experience</h1>
+            <p class="text mt-0 text-muted mb-0">I've gotten the opportunity to work with some of the most innovative and trusted companies, in helping to contribute to the development, design, and usability of new web and digital experiences. My work has been proven successful time and time again.</p>
+        </div>
+
+        <hr class="my-5">
+
+            <div class="card p-4 ni">
+                <div class="card-body">
+                    <img width="50"
+                         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEwmBs4BzmGhMKA5QqMrU6cu10b5yxscJHjqmgxILeD-_wXWejXQweii6YRgynHuR9enU&usqp=CAU"
+                         class="rounded mb-2" alt="">
+
+                    <div class="d-flex justify-content-between mb-2">
+                        <div>
+                            <div>
+                                <div>
+                                    <h4 class="mb-2">Web
+                                        Developer
+                                        Intern</h4>
+                                    <p class="x-small text-muted mb-0">Snap-on Incorporated</p>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div>
+
+                            <p class="small mb-2 text-end">May 2021 - present</p>
+                            <p class="small text-end">Kenosha, Wisconsin</p>
+
+                        </div>
+
+                    </div>
+
+
+                    <div class="d-flex">
+                        <div class="me-2 ni">✨</div>
+                        <p class="mb-2">Accelerated the design, development, and deployment of 3 new
+                            internal applications</p>
+                    </div>
+
+                    <div class="d-flex">
+                        <div class="me-2 ni">✨</div>
+                        <p class="mb-2">Organized and participated in device enrollment and roll out
+                            of
+                            application pilot program to 40+ iPad devices</p>
+                    </div>
+
+                    <div class="d-flex">
+                        <div class="me-2 ni">✨</div>
+                        <p class="mb-2">Diagnosed slow and demanding SQL queries to improve
+                            application
+                            load
+                            times and performance</p>
+                    </div>
+
+                    <div class="d-flex">
+                        <div class="me-2 ni">✨</div>
+                        <p class="mb-2">Identified potential security threats and concerns in
+                            infrastructure</p>
+                    </div>
+
+                    <div class="d-flex">
+                        <div class="me-2 ni">✨</div>
+                        <p class="mb-2">Performed regular audits to ensure code is error-free and
+                            meets
+                            conventional standards</p>
+                    </div>
+
+                    <div class="d-flex">
+                        <div class="me-2 ni">✨</div>
+                        <p class="mb-2">Implemented new features and code modifications based on
+                            in-field user
+                            feedback</p>
+                    </div>
+
+                    <div class="d-flex">
+                        <div class="me-2 ni">✨</div>
+                        <p class="mb-2">Integrated multiple new API endpoints to retrieve customer
+                            data
+                            and
+                            insights</p>
+                    </div>
+
+                    <div class="d-flex">
+                        <div class="me-2 ni">✨</div>
+                        <p class="mb-2">Collaborated with team members to discuss new ideas and
+                            implementations</p>
+                    </div>
+
+                </div>
+            </div>
+
+        <div class="card p-4 ni mt-4">
+            <div class="card-body">
+
+                <div class="d-flex justify-content-between mb-2">
+                    <div>
+                        <div>
+                            <div>
+                                <h4 class="mb-2">Tech Ed Intern</h4>
+                                <p class="x-small text-muted mb-0">Franklin High School</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div>
+
+                        <p class="small mb-2 text-end">2019 - 2020</p>
+                        <p class="small text-end">Franklin, Wisconsin</p>
+
+                    </div>
+
+                </div>
+
+                <div class="d-flex">
+                    <div class="me-2 ni">✨</div>
+                    <p class="mb-2">Coordinated assignments and tasks for 25+ peers</p>
+                </div>
+
+                <div class="d-flex">
+                    <div class="me-2 ni">✨</div>
+                    <p class="mb-2">Budgeted and planned total costs for specific projects</p>
+                </div>
+
+                <div class="d-flex">
+                    <div class="me-2 ni">✨</div>
+                    <p class="mb-2">Assessed student performance to determine requirements were met</p>
+                </div>
+
+                <div class="d-flex">
+                    <div class="me-2 ni">✨</div>
+                    <p class="mb-2">Addressed questions and concerns raised by peers</p>
+                </div>
+
+                <div class="d-flex">
+                    <div class="me-2 ni">✨</div>
+                    <p class="mb-2">Observed progress and overall growth</p>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="card p-4 ni mt-4">
+            <div class="card-body">
+
+                <div class="d-flex justify-content-between mb-2">
+                    <div>
+                        <div>
+                            <div>
+                                <h4 class="mb-2">Store Cashier</h4>
+                                <p class="x-small text-muted mb-0">Franklin High School</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div>
+
+                        <p class="small mb-2 text-end">2016 - 2020</p>
+                        <p class="small text-end">Franklin, Wisconsin</p>
+
+                    </div>
+
+                </div>
+
+                <div class="d-flex">
+                    <div class="me-2 ni">✨</div>
+                    <p class="mb-2">Trained new student employees on the ShopKeep POS system</p>
+                </div>
+
+                <div class="d-flex">
+                    <div class="me-2 ni">✨</div>
+                    <p class="mb-2">Developed new marketing techniques to increase sales</p>
+                </div>
+
+                <div class="d-flex">
+                    <div class="me-2 ni">✨</div>
+                    <p class="mb-2">Organized stock inventory</p>
+                </div>
+
+                <div class="d-flex">
+                    <div class="me-2 ni">✨</div>
+                    <p class="mb-2">Obtained feedback from customers in order to improve experience</p>
+                </div>
+
+                <div class="d-flex">
+                    <div class="me-2 ni">✨</div>
+                    <p class="mb-2">Evaluated performance and sales with peers during monthly meetings</p>
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+
+    <?= Loader::getFooter() ?>
+
+</div>
+
+<?= Loader::getScripts() ?>
+</body>
+</html>
