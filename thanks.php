0e9da0af0394a14545487172e750e5623dd68567
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-20 21:46:34 -0500

    Fixed button icon margins

--- thanks.php
+++ thanks.php
@@ -1,4 +1,4 @@
-<?php require_once "classes/Loader.php" ?>
+<?php require_once "config.php" ?>
 <!DOCTYPE html>
 <html lang="en">
 <?= Loader::getHead('Thank you') ?>
@@ -11,7 +11,7 @@
         <p class="mb-3">Your message has arrived safe and sound in my inbox.</p>
         <p class="mt-0">I'll get back to you as soon as possible. Talk soon!</p>
         <a href="./" class="button-3 primary w-button">Continue to homepage <i
-                    class="far fa-long-arrow-right me-2"></i></a>
+                    class="far fa-long-arrow-right ms-2"></i></a>
     </div>
     <?= Loader::getScripts() ?>
 </body>
