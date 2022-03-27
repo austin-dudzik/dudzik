4c56a05ad779389476b5d451778bad70e75fa361
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-27 15:00:16 -0500

    Updated coming soon layout

--- coming-soon.php
+++ coming-soon.php
@@ -1,17 +1,39 @@
-<?php require_once "classes/Loader.php" ?>
+<?php require_once "config.php" ?>
 <!DOCTYPE html>
 <html lang="en">
 <?= Loader::getHead('Coming Soon') ?>
 <body>
 <?= Loader::getExtras() ?>
-<?= Loader::getNavigation() ?>
-<div class="container-fluid text-center">
+<div class="container text-start text-md-center px-4 px-md-0" id="coming-soon">
     <h1 class="heading heading-lg ni mb-4">Coming Soon.</h1>
-    <p class="text mb-3">At the moment, I'm currently working on a new homepage.</p>
-    <p class="text mb-4">Instead, how about checking out my homepage?</p>
-    <a href="https://dudzik.dev" class="button-3 primary w-button mt-2">
-        Continue to homepage <i class="far fa-long-arrow-right ms-2"></i>
-    </a>
+    <p class="text mb-5">I'm currently working on something new that isn't quite ready yet.</p>
+
+    <hr>
+
+    <p class="text my-5">In the meantime, why not take a second to explore me elsewhere on the internet?</p>
+
+        <div class="row social-items mt-4">
+            <div class="col-12 col-md mb-3 text-start text-md-center">
+                <a href="<?= $social["github"] ?>" target="_blank">
+                    <i class="fab fa-github me-2"></i> GitHub
+                </a>
+            </div>
+            <div class="col-12 col-md mb-3 text-start text-md-center">
+                <a href="<?= $social["linkedin"] ?>" target="_blank">
+                    <i class="fab fa-linkedin me-2"></i> LinkedIn
+                </a>
+            </div>
+            <div class="col-12 col-md mb-3 text-start text-md-center">
+                <a href="<?= $social["dribbble"] ?>" target="_blank">
+                    <i class="fab fa-dribbble me-2"></i> Dribbble
+                </a>
+            </div>
+            <div class="col-12 col-md-4 mb-3 text-start text-md-center">
+                <a href="mailto:<?= $email ?>" target="_blank">
+                    <i class="fas fa-envelope me-3"></i><?= $email ?>
+                </a>
+            </div>
+    </div>
 </div>
 <?= Loader::getScripts() ?>
 </body>
