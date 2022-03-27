4447715b8bffd120d3b7836c2c31c20e4e311a56
Austin Dudzik <65981261+austin-dudzik@users.noreply.github.com>
2022-03-27 14:59:15 -0500

    Adjusted container and button

--- thanks.php
+++ thanks.php
@@ -5,14 +5,12 @@
 <body>
 <?= Loader::getExtras() ?>
 <?= Loader::getNavigation() ?>
-<div class="container text-center">
-    <div class="intro">
+<div class="container text-start text-md-center" id="thanks">
         <h1 class="gradient-heading ni mb-4">Thank you!</h1>
         <p class="mb-3">Your message has arrived safe and sound in my inbox.</p>
         <p class="mt-0">I'll get back to you as soon as possible. Talk soon!</p>
-        <a href="./" class="button-3 primary w-button">Continue to homepage <i
+        <a href="<?= $site_url ?>" class="submit-button">Continue to homepage <i
                     class="far fa-long-arrow-right ms-2"></i></a>
-    </div>
     <?= Loader::getScripts() ?>
 </body>
 </html>
